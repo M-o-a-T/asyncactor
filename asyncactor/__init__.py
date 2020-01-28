59fd5b34c56a85e807f169482020465a2f8a821c
Matthias Urlichs <matthias@urlichs.de>
2020-01-28 19:13:16 +0100

    Split off node list

--- asyncactor/__init__.py
+++ asyncactor/__init__.py
@@ -1,7 +1,9 @@
 from .actor import *
 from .events import *
+from .nodelist import *
 from .backend import get_transport
 from .actor import __all__ as _actor_all
 from .events import __all__ as _events_all
-__all__ = _actor_all + _events_all + ['get_transport']
+from .nodelist import __all__ as _nodelist_all
+__all__ = _actor_all + _events_all + _nodelist_all + ['get_transport']
 
