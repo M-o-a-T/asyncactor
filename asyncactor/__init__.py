cb6216d60bb3afd2f1197ef9f63a6e93979e46df
Matthias Urlichs <matthias@urlichs.de>
2020-01-28 19:09:28 +0100

    Move events to their own file

--- asyncactor/__init__.py
+++ asyncactor/__init__.py
@@ -1,5 +1,7 @@
 from .actor import *
+from .events import *
 from .backend import get_transport
 from .actor import __all__ as _actor_all
-__all__ = _actor_all + ['get_transport']
+from .events import __all__ as _events_all
+__all__ = _actor_all + _events_all + ['get_transport']
 
--- asyncactor/actor.py
+++ asyncactor/actor.py
@@ -8,163 +8,12 @@ from contextlib import asynccontextmanager
 
 from .abc import Transport
 from .exceptions import ActorTimeoutError, ActorCollisionError
+from .events import *
 
 __all__ = [
         "Actor",
-        "NodeEvent",
-        "AuthPingEvent",
-        "TagEvent",
-        "UntagEvent",
-        "DetagEvent",
-        "RawPingEvent",
-        "PingEvent",
-        "GoodNodeEvent",
-        "RecoverEvent",
         "NodeList",
     ]
-
-class NodeEvent:
-    pass
-
-
-class AuthPingEvent(NodeEvent):
-    """
-    Superclass for tag and ping: must arrive within :meth:`cycle_time_max` seconds of each other.
-
-    Non-abstract subclasses of this must have ``name`` and ``value`` attributes.
-    (TODO: enforce this)
-    """
-
-    pass
-
-
-class TagEvent(AuthPingEvent):
-    """
-    This event says that for the moment, you're "it".
-    """
-
-    def __init__(self, name, value):
-        self.node = name
-        self.value = value
-
-    def __repr__(self):
-        return "<Tag>"
-
-
-class UntagEvent(NodeEvent):
-    """
-    Your tag cycle time has passed. You're no longer "it".
-    """
-
-    def __repr__(self):
-        return "<UnTag>"
-
-
-class DetagEvent(UntagEvent):
-    """
-    A ping from another node has arrived while you're "it".
-    Unfortunately, it is "better" than ours.
-
-    Arguments:
-      node (str): The node that superseded us.
-    """
-
-    def __init__(self, node):
-        self.node = node
-
-    def __repr__(self):
-        return "<DeTag %r>" % (self.node,)
-
-
-class RawPingEvent(NodeEvent):
-    """
-    A ping from another node shows up. Not yet filtered!
-
-    Arguments:
-      msg (dict): The ping message of the currently-active actor.
-    """
-
-    def __init__(self, msg):
-        self.msg = msg
-
-    def __repr__(self):
-        return "<RawPing %r>" % (self.msg,)
-
-
-class PingEvent(AuthPingEvent):
-    """
-    A ping from another node shows up: the node ``.node`` is "it".
-
-    Arguments:
-      msg (dict): The ping message of the currently-active actor.
-    """
-
-    def __init__(self, msg):
-        self.msg = msg
-
-    def __repr__(self):
-        return "<Ping %r>" % (self.msg,)
-
-    @property
-    def node(self):
-        """
-        Name of the node. Shortcut to ``msg['node']``.
-        """
-        return self.msg["node"]
-
-    @property
-    def value(self):
-        """
-        Name of the node. Shortcut to ``msg['node']``.
-        """
-        try:
-            return self.msg["value"]
-        except KeyError:
-            return None
-
-
-class GoodNodeEvent(NodeEvent):
-    """
-    A known-good node has been seen. We might want to get data from it.
-
-    Arguments:
-      nodes (list(str)): Nodes known to have a non-``None`` value.
-
-    This event is seen while starting up, when our value is ``None``.
-    """
-
-    def __init__(self, nodes):
-        self.nodes = nodes
-
-    def __repr__(self):
-        return "<Good %r>" % (self.nodes,)
-
-
-class RecoverEvent(NodeEvent):
-    """
-    We need to recover from a network split.
-
-    Arguments:
-      prio: Our recovery priority. Zero is highest.
-      replace: Flag whether the other side has superseded ours.
-      local_nodes: A list of recent actors on our side.
-      remote_nodes: A list of recent actors on the other side.
-    """
-
-    def __init__(self, prio, replace, local_nodes, remote_nodes):
-        self.prio = prio
-        self.replace = replace
-        self.local_nodes = local_nodes
-        self.remote_nodes = remote_nodes
-
-    def __repr__(self):
-        return "<Recover %d %s %r %r>" % (
-            self.prio,
-            self.replace,
-            self.local_nodes,
-            self.remote_nodes,
-        )
-
 
 class NodeList(list):
     """
@@ -201,7 +50,7 @@ class NodeList(list):
     >>> n += "c"
     >>> n
     ['c', 'd']
-    >>>
+    >>> 
     """
 
     def __init__(self, maxlen, data=(), mutable=False):
