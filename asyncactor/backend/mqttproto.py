d75a57ad4b6e32d8ead0d0ed18c0c8b1457e0159
Matthias Urlichs <matthias@urlichs.de>
2024-11-19 09:29:09 +0100

    Add mqttproto transport

---
+++ asyncactor/backend/mqttproto.py
@@ -0,0 +1,66 @@
+"""
+Listener on top of an async mqttproto connection
+"""
+from __future__ import annotations
+
+from asyncactor.abc import Transport, MonitorStream
+from moat.util import create_queue, Path, CtxObj
+from mqttproto import QoS
+from moat.lib.codec import get_codec
+
+from mqttproto.async_client import AsyncMQTTClient
+
+
+class MQTTPTransport(Transport):
+    """
+    MQTTProto Transport for AsyncActor.
+    """
+
+    def __init__(self, conn: AsyncMQTTClient, topic: Path, codec="cbor"):
+        self.conn = conn
+        self.topic = topic
+        self.tag = "/".join(topic)
+        self.codec = get_codec(codec)
+
+    def monitor(self):
+        """
+        Accept incoming messages::
+
+            async with conn.monitor() as m:
+                async for msg in m:
+                    await process(msg)
+        """
+        return MQTTMonitor(self)
+
+    def send(self, payload) -> Awaitable[None]:
+        """
+        Send a message.
+        """
+        return self.conn.publish(self.tag, self.codec.encode(payload), qos=QoS.AT_LEAST_ONCE)
+
+    def __repr__(self):
+        return "<MQTT:%s @%r>" % (self.tag, self.conn)
+
+
+class MQTTMonitor(MonitorStream, CtxObj):
+    async def _ctx(self):
+        c=self.transport
+        async with c.conn.subscribe(c.tag, maximum_qos=QoS.AT_LEAST_ONCE) as mon:
+            self._it = aiter(mon)
+            try:
+                yield self
+            finally:
+                self._it = None
+
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        msg = self.transport.codec.decode((await anext(self._it)).payload)
+        return msg
+
+    def __repr__(self):
+        return "<Mon:%r>" % (self.transport,)
+
+
+Transport = MQTTPTransport
---
+++ tests/test_mqttproto.py
@@ -0,0 +1,122 @@
+import pytest
+import trio
+import os
+import time
+
+from asyncactor.actor import (
+    Actor,
+    GoodNodeEvent,
+    TagEvent,
+    UntagEvent,
+    PingEvent,
+)
+from asyncactor.backend import get_transport
+from mqttproto.async_client import AsyncMQTTClient
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+N = 20
+
+
+Config = {}
+
+def _env(s, d, f=lambda x: x):
+    es = "MQTT_" + s.upper()
+    es = os.environ.get(es, None)
+    Config[s] = f(es) if es is not None else d
+
+_env("host", "localhost")
+_env("port", 1883, int)
+
+
+@pytest.mark.trio
+async def test_20_all():
+    """
+    This test starts multiple servers at the same time and checks that all
+    of them get their turn.
+    """
+    N = 5  # pylint: disable=redefined-outer-name
+    tagged = False
+    msgs = {}
+
+    async def s1(i, *, task_status):
+        nonlocal tagged
+        async with AsyncMQTTClient(Config["host"], Config["port"], client_id="act_test_%d" % (i,)) as C:
+            T = get_transport("mqttproto")(C, "test_20")
+            async with Actor(T, "c_" + str(i), cfg={"nodes": N, "gap": 0.1, "cycle": 1}) as k:
+                task_status.started()
+                await k.set_value(i * 31)
+                c = 0
+                t = time.time()
+                async for m in k:
+                    if i == 1:
+                        k.logger.info("*** MSG %d %r", i, m)
+                    ot, t = t, time.time()
+                    if ot != t:
+                        assert tagged <= 1
+                    msgs.setdefault(i, []).append(m)
+                    if isinstance(m, GoodNodeEvent):
+                        pass
+                    elif isinstance(m, TagEvent):
+                        # assert not tagged  # may collide, so checked above
+                        tagged += 1
+                        c += 1
+                    elif isinstance(m, UntagEvent):
+                        assert tagged
+                        tagged -= 1
+                        if c > 2:
+                            break
+                assert tagged <= 1
+                k.logger.debug("N2 %r", k._values)
+                for x in range(1, 6):
+                    assert k._values["c_" + str(x)] == x * 31
+
+    async with trio.open_nursery() as tg:
+        for i in range(1, 6):
+            await tg.start(s1, i)
+
+
+@pytest.mark.trio
+async def test_21_some():
+    """
+    This test starts multiple servers at the same time and checks that
+    some of them are skipped.
+    """
+    N = 15  # pylint: disable=redefined-outer-name
+
+    msgs = {}
+
+    c = 0
+    h = [0] * (N + 1)
+
+    async def s1(i, *, task_status):
+        async with AsyncMQTTClient(Config["host"], Config["port"], client_id="act_test_%d" % (i,)) as C:
+            T = get_transport("mqttproto")(C, "test_21")
+            nonlocal c
+            async with Actor(T, "c_" + str(i), cfg={"nodes": 3, "gap": 0.1, "cycle": 1}) as k:
+                task_status.started()
+                await k.set_value(i * 31)
+                async for m in k:
+                    msgs.setdefault(i, []).append(m)
+                    if i == 1:
+                        k.logger.info("*** MSG %d %r", i, m)
+                    if isinstance(m, GoodNodeEvent):
+                        pass
+                    elif isinstance(m, TagEvent):
+                        c += 1
+                        h[i] += 1
+                    elif isinstance(m, (PingEvent, UntagEvent)):
+                        if c > 10:
+                            assert sum((x > 0) for x in h) < 6
+                            return
+            for i in range(1, 6):
+                assert k._values["c_" + str(i)] == i * 31
+
+    async with trio.open_nursery() as tg:
+        for i in range(1, 6):
+            await tg.start(s1, i)
+
+        await trio.sleep(10)
+    pass  # server end
