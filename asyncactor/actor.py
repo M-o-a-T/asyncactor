a120699b63a390887d349bc022465864e1031a89
Matthias Urlichs <matthias@urlichs.de>
2025-04-18 15:36:30 +0200

    another anyio 2-ism

--- asyncactor/actor.py
+++ asyncactor/actor.py
@@ -716,7 +716,7 @@ class Actor(CtxObj):
         """
         # For pos=0 this is a no-op and times out immediately
         try:
-            async with anyio.open_cancel_scope() as xx:
+            async with anyio.CancelScope() as xx:
                 if self._version_job is not None:
                     self._version_job.cancel()
                 self._version_job = xx
